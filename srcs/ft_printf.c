5c6999285313814879960c5576ebe661728f300c
aburnott <aburnott@student.s19.be>
2022-10-15 02:21:31 +0200

    printf changes

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,9 +6,19 @@
 /*   By: aburnott <aburnott@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 14:43:34 by aburnott          #+#    #+#             */
-/*   Updated: 2022/10/14 14:43:54 by aburnott         ###   ########.fr       */
+/*   Updated: 2022/10/14 18:10:40 by aburnott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "ft_printf.h"
+
+char	check_type(const char *str)
+{
+	
+}
+
+int	ft_printf(const char *str, ...)
+{
+	return (0);
+}
