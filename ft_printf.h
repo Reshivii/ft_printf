26c20b8464f36dced61995096a85ad834cb9a865
aburnott <aburnott@student.s19.be>
2022-10-18 16:44:13 +0200

    rn libft

--- ft_printf.h
+++ ft_printf.h
@@ -6,16 +6,16 @@
 /*   By: aburnott <aburnott@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 14:29:27 by aburnott          #+#    #+#             */
-/*   Updated: 2022/10/18 13:44:25 by aburnott         ###   ########.fr       */
+/*   Updated: 2022/10/18 16:43:51 by aburnott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include "libft.h"
 # include <stdarg.h>
 # include <unistd.h>
+# include <stdlib.h>
 
 typedef struct s_data
 {
--- ft_utils.c
+++ ft_utils.c
@@ -6,8 +6,34 @@
 /*   By: aburnott <aburnott@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 13:43:38 by aburnott          #+#    #+#             */
-/*   Updated: 2022/10/18 13:44:00 by aburnott         ###   ########.fr       */
+/*   Updated: 2022/10/18 16:43:19 by aburnott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return ((char *)s + i);
+		i++;
+	}
+	if (s[i] == (char)c)
+		return ((char *)s + i);
+	return (0);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	int	size;
+
+	size = 0;
+	while (s[size])
+		size++;
+	return (size);
+}
