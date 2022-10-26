b7472ceb549dc3e5fd462f1ceb36a33274eb821c
aburnott <aburnott@student.s19.be>
2022-10-26 14:44:26 +0200

    rm main.c

--- ft_printf_bonus.h
+++ ft_printf_bonus.h
@@ -1,21 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_printf_bonus.h                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aburnott <aburnott@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/25 18:58:01 by aburnott          #+#    #+#             */
-/*   Updated: 2022/10/26 13:42:04 by aburnott         ###   ########.fr       */
+/*   Created: 2022/10/25 21:56:19 by aburnott          #+#    #+#             */
+/*   Updated: 2022/10/26 14:27:39 by aburnott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include "ft_printf.h"
-#include <limits.h>
+#ifndef FT_PRINTF_BONUS_H
+# define FT_PRINTF_BONUS_H
 
-int	main(void)
-{
-	ft_printf("%%%%");
-	return (0);
-}
+# include <stdarg.h>
+# include <unistd.h>
+
+int	ft_printf(const char *s, ...);
+int	ft_putchar(char c);
+int	ft_putstr(char *s);
+int	ft_putnbr(long nb, int len);
+int	ft_hexa(unsigned long hex, int len, int type);
+int	ft_void_hexa(void *p, int type);
+
+#endif
